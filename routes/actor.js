b8b976eabefde19ec97562b90d94d1dc86ef6c5b
marusoft <alimi.kmaruf@gmail.com>
2020-07-18 12:01:57 +0100

    add routes for actors

--- routes/actor.js
+++ routes/actor.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const ActorController = require('../controllers/actors');
+
 // Routes related to actor.
+router.put('/', ActorController.updateActor);
+router.get('/', ActorController.getAllActors);
+router.get('/streak', ActorController.getStreak);
 
 module.exports = router;
